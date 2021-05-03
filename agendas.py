88dccb5999210d3bb319baf4a47d8717364f6938
Yigit Guneli <yigiit@gmail.com>
2021-05-04 01:23:33 +0200

    cleanup

---
+++ agendas.py
@@ -0,0 +1,20 @@
+import requests
+from collections import defaultdict
+
+
+def group_agendas(agendas):
+    agenda_ids = defaultdict(lambda: defaultdict(list))
+
+    for agenda in agendas:
+        if agenda['booking_disabled'] or agenda['booking_temporary_disabled']:
+            continue
+
+        if not agenda['visit_motive_ids']:
+            continue
+
+        practice_id = agenda['practice_id']
+        motive_id = agenda['visit_motive_ids'][0]
+
+        agenda_ids[practice_id][motive_id].append(agenda['id'])
+
+    return agenda_ids
